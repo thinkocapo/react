4baabb68e2ecc9f178074bb23aa744534d701436
William Capozzoli <thinkocapo@gmail.com>
2019-08-07 21:55:15 -0700

    structure 1 2 3 4

--- react.sh
+++ react.sh
@@ -9,27 +9,48 @@ LARAVEL='$HOME/path/to/laravel'
 RAILS='$HOME/path/to/rails'
 SPRING='$HOME/path/to/spring'
 
+# 1 Initialize Tmux split window
+# tmux new-session -s demos \; 
+# tmux split-window -v\; 
 
+echo 'echo in between Step 1 and Step 2'
+
+# 2 Start React in one of the window panes
+# React ( Directory1 & Command1)
+# tmux send-keys -t 0 'cd ~/thinkocapo/react' C-m \;
+# tmux send-keys -t 0 'npm run deploy' C-m \;
+
+echo 'echo in between Step 2 and Step 3'
+
+
+# 3 Start The Server
+# TODO - need right run command
 if [[ $1 == 'aspnet' ]]
-    then REACT_APP_PORT=5001 npm run deploy;
+    then REACT_APP_PORT=5001 tmux send-keys -t 1 'cd ~/thinkocapo/flask' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
 
 if [[ $1 == 'express' ]]
-    then REACT_APP_PORT=3001 npm run deploy;
+    then REACT_APP_PORT=3001 tmux send-keys -t 1 'cd ~/thinkocapo/express' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
 
 if [[ $1 == 'flask' ]]
-    then REACT_APP_PORT=5001 npm run deploy;
+    then REACT_APP_PORT=5001 tmux send-keys -t 1 'cd ~/thinkocapo/flask' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
 
 if [[ $1 == 'laravel' ]]
-    then REACT_APP_PORT=8000 npm run deploy;
+    then REACT_APP_PORT=8000 tmux send-keys -t 1 'cd ~/thinkocapo/laravel' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
 
 if [[ $1 == 'rails' ]]
-    then REACT_APP_PORT=3001 npm run deploy;
+    then REACT_APP_PORT=3001 tmux send-keys -t 1 'cd ~/thinkocapo/rails' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
 
 if [[ $1 == 'spring' ]]
-    then REACT_APP_PORT=8080 npm run deploy;
+    then REACT_APP_PORT=8080 tmux send-keys -t 1 'cd ~/thinkocapo/spring?' C-m \; tmux send-keys -t 1 'make deploy' C-m \;
 fi
+
+
+
+# TODO
+## make it so `react.sh react` only starts react app, no server side.
+## make it so can only run server side, but that sounds like a separate script altogether. or could use an alias for that, easy.
